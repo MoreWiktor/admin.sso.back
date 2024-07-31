843a2ed8654ceb32f17c6d5e45c4882dc0d4d58a
MoreWiktor <MoreWiktor@yandex.ru>
2024-07-31 15:41:12 +0300

    fix(user) change user unique constraint to email and company_id

--- src/core/user/entities/user.entity.ts
+++ src/core/user/entities/user.entity.ts
@@ -7,15 +7,17 @@ import {
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity({ name: 'users' })
+@Unique('UQ_97672ac88f789774dd47f7c8be3', ['email', 'company'])
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true })
+  @Column()
   email: string;
 
   @Column()
